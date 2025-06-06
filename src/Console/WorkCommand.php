5913caabf5e454a0399cb57b56ae9f9e4d86601c
Michael A <39175160+michaelthedev@users.noreply.github.com>
2025-06-06 08:19:51 +0100

    cleanup WorkCommand

--- src/Console/WorkCommand.php
+++ src/Console/WorkCommand.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace LogadApp\Queue\Console;
 
+use LogadApp\Queue\Worker;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -15,55 +17,19 @@ use Symfony\Component\Console\Style\SymfonyStyle;
     description: 'Process jobs from the queue.',
     hidden: false
 )]
-final class Work extends Command
+final class WorkCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
-        $io->writeln("Starting queue worker...");
+        $worker = new Worker;
+        $logger = function($message) use ($io) {
+            $io->writeln("[" . date('Y-m-d H:i:s') . "] {$message}");
+        };
 
-		while (true) {
-			// Check memory usage
-			if ($this->memoryExceeded()) {
-				$output->writeln('Memory limit exceeded. Restarting worker...');
-				break;
-			}
-
-			// Process jobs
-			$this->processNextJob($output);
-
-			// Sleep for a while before checking for new jobs
-			sleep(1);
-		}
+        $worker->work('default', $logger);
 
         return Command::SUCCESS;
     }
-
-	protected function processNextJob(OutputInterface $output)
-	{
-		$job = Capsule::table('jobs')
-			->where('queue', 'default')
-			->where('available_at', '<=', time())
-			->whereNull('reserved_at')
-			->orderBy('id', 'asc')
-			->first();
-
-		if ($job) {
-			// Mark the job as reserved
-			Capsule::table('jobs')
-				->where('id', $job->id)
-				->update(['reserved_at' => time()]);
-
-			// Unserialize and process the job
-			try {
-				$jobInstance = unserialize($job->payload);
-				$jobInstance->id = $job->id;
-				$jobInstance->attempts = $job->attempts;
-				$jobInstance->process();
-			} catch (Exception $e) {
-				$output->writeln("Job failed: " . $e->getMessage());
-			}
-		}
-	}
 }
