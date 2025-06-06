06a6aed17cfe4e7e8634ec8a46d2e3c11df384dc
Michael A <39175160+michaelthedev@users.noreply.github.com>
2025-06-06 08:23:30 +0100

    fix: incomplete namespace + methods

--- src/Contracts/StoreInterface.php
+++ src/Contracts/StoreInterface.php
@@ -2,18 +2,24 @@
 
 declare(strict_types=1);
 
-namespace LogadApp\Queue;
+namespace LogadApp\Queue\Contracts;
 
 interface StoreInterface
 {
 	/**
-	 * @param string $queue
-	 * @param string $payload
+	 * @param string $queue queue name
+	 * @param string $payload The serialized job w/o payload
 	 * @return string The unique ID of the job
 	 */
 	public function add(string $queue, string $payload): string;
 
+	/**
+	 * @param string $queue queue name
+	 * @return array|null
+	 */
 	public function next(string $queue): ?array;
 
 	public function delete(string $queue, string $uid): void;
+
+	public function retry(string $queue, string $payload): string;
 }
